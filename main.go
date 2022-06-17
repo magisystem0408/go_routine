5017578571bd2e2c38d09db2a85e48a1ebadca41
magisystem0408 <masato.matsudo@gmail.com>
2022-06-17 14:43:34 +0900

    chanel range & close

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func goroutine(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+		c <- sum
+	}
+	close(c)
+}
+func main() {
+	s := []int{1, 2, 3, 4, 5}
+	c := make(chan int)
+	go goroutine(s, c)
+	for i := range c {
+		fmt.Println(i)
+	}
+}
