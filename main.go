e39fce9b24451212678d0741508b551326148109
magisystem0408 <masato.matsudo@gmail.com>
2022-06-17 16:52:33 +0900

    sync/Mutex

--- main.go
+++ main.go
@@ -2,27 +2,46 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-func main() {
-	//tickとafterはチャネルを送る
-	tick := time.Tick(100 * time.Millisecond)
-	boom := time.After(500 * time.Millisecond)
-OuterLoop:
-	for {
-		select {
-		case t := <-tick:
-			fmt.Println("tick.", t)
-		case <-boom:
-			fmt.Println("BOOM!")
-			break OuterLoop
-			return
+type Counter struct {
+	v   map[string]int
+	mux sync.Mutex
+}
 
-		//	二つのチャネル以外からきたものを扱う
-		default:
-			fmt.Println("    .")
-			time.Sleep(50 * time.Millisecond)
+func (c *Counter) Inc(key string) {
+	//lockをしてからmux
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	c.v[key]++
+}
+
+func (c *Counter) Value(key string) int {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	return c.v[key]
+}
+
+func main() {
+	//二つのgo routineから一つのmapを書き換えようとするとエラーが出る
+	//c := make(map[string]int)
+	c := Counter{v: make(map[string]int)}
+
+	//go routine
+	go func() {
+		for i := 0; i < 10; i++ {
+			c.Inc("Key")
 		}
-	}
+	}()
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			c.Inc("Key")
+		}
+	}()
+
+	time.Sleep(time.Second * 1)
+	fmt.Println(c, c.Value("key"))
 }
